7fa27efa37c92a4bf90ee10d5340e0c7628152ac
tilwa <shahrukhaltaf123@gmail.com>
2025-02-17 20:50:36 +0500

    redux mini bank

--- src/store-v2.js
+++ src/store-v2.js
@@ -1,14 +1,13 @@
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+
 import { configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "@redux-devtools/extension"; // Updated import
 
 const store = configureStore({
   reducer: {
     account: accountReducer,
     customer: customerReducer,
   },
-  devTools: composeWithDevTools(), // Enable Redux DevTools
 });
 
 export default store;
